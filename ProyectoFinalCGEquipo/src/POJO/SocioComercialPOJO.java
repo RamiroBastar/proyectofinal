8f47d72d47d37c104c7b8c249af61e95332cb9aa
Profesor Bastar <Profesor Bastar@DESKTOP-JPSLELM>
2016-12-17 16:14:24 -0600

    Creé socio comercial pojo

---
+++ ProyectoFinalCGEquipo/src/POJO/SocioComercialPOJO.java
@@ -0,0 +1,145 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package POJO;
+
+import java.awt.Image;
+import java.sql.Date;
+
+/**
+ *
+ * @author Profesor Bastar
+ */
+public class SocioComercialPOJO {
+
+    private int idSocioComercial;
+    private String nombre_socio_comercial;
+    private String rfc_socio_comercial;
+    private String razon_social_socio_comercial;
+    private String direccion_socio_comercial;
+    private String localidad_socio_comercial;
+    private String municipio_socio_comercial;
+    private String estado_socio_comercial;
+    private String colonia_socio_comercial;
+    private String cp_socio_comercial;
+    private String pais_socio_comercial;
+    private String cliente_o_proveedor_socio_comercial;
+    private Date fecha_nacimiento_socio_comercial;
+    private Image foto_socio_comercial;
+
+    public int getIdSocioComercial() {
+        return idSocioComercial;
+    }
+
+    public void setIdSocioComercial(int idSocioComercial) {
+        this.idSocioComercial = idSocioComercial;
+    }
+
+    public String getNombre_socio_comercial() {
+        return nombre_socio_comercial;
+    }
+
+    public void setNombre_socio_comercial(String nombre_socio_comercial) {
+        this.nombre_socio_comercial = nombre_socio_comercial;
+    }
+
+    public String getRfc_socio_comercial() {
+        return rfc_socio_comercial;
+    }
+
+    public void setRfc_socio_comercial(String rfc_socio_comercial) {
+        this.rfc_socio_comercial = rfc_socio_comercial;
+    }
+
+    public String getRazon_social_socio_comercial() {
+        return razon_social_socio_comercial;
+    }
+
+    public void setRazon_social_socio_comercial(String razon_social_socio_comercial) {
+        this.razon_social_socio_comercial = razon_social_socio_comercial;
+    }
+
+    public String getDireccion_socio_comercial() {
+        return direccion_socio_comercial;
+    }
+
+    public void setDireccion_socio_comercial(String direccion_socio_comercial) {
+        this.direccion_socio_comercial = direccion_socio_comercial;
+    }
+
+    public String getLocalidad_socio_comercial() {
+        return localidad_socio_comercial;
+    }
+
+    public void setLocalidad_socio_comercial(String localidad_socio_comercial) {
+        this.localidad_socio_comercial = localidad_socio_comercial;
+    }
+
+    public String getMunicipio_socio_comercial() {
+        return municipio_socio_comercial;
+    }
+
+    public void setMunicipio_socio_comercial(String municipio_socio_comercial) {
+        this.municipio_socio_comercial = municipio_socio_comercial;
+    }
+
+    public String getEstado_socio_comercial() {
+        return estado_socio_comercial;
+    }
+
+    public void setEstado_socio_comercial(String estado_socio_comercial) {
+        this.estado_socio_comercial = estado_socio_comercial;
+    }
+
+    public String getColonia_socio_comercial() {
+        return colonia_socio_comercial;
+    }
+
+    public void setColonia_socio_comercial(String colonia_socio_comercial) {
+        this.colonia_socio_comercial = colonia_socio_comercial;
+    }
+
+    public String getCp_socio_comercial() {
+        return cp_socio_comercial;
+    }
+
+    public void setCp_socio_comercial(String cp_socio_comercial) {
+        this.cp_socio_comercial = cp_socio_comercial;
+    }
+
+    public String getPais_socio_comercial() {
+        return pais_socio_comercial;
+    }
+
+    public void setPais_socio_comercial(String pais_socio_comercial) {
+        this.pais_socio_comercial = pais_socio_comercial;
+    }
+
+    public String getCliente_o_proveedor_socio_comercial() {
+        return cliente_o_proveedor_socio_comercial;
+    }
+
+    public void setCliente_o_proveedor_socio_comercial(String cliente_o_proveedor_socio_comercial) {
+        this.cliente_o_proveedor_socio_comercial = cliente_o_proveedor_socio_comercial;
+    }
+
+    public Date getFecha_nacimiento_socio_comercial() {
+        return fecha_nacimiento_socio_comercial;
+    }
+
+    public void setFecha_nacimiento_socio_comercial(Date fecha_nacimiento_socio_comercial) {
+        this.fecha_nacimiento_socio_comercial = fecha_nacimiento_socio_comercial;
+    }
+
+    public Image getFoto_socio_comercial() {
+        return foto_socio_comercial;
+    }
+
+    public void setFoto_socio_comercial(Image foto_socio_comercial) {
+        this.foto_socio_comercial = foto_socio_comercial;
+    }
+    
+
+}
